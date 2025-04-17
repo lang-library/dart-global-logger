7cf266c504b8bf77ca562723bd8da89994592bbe
JavaCommons Technologies <javacommons@gmail.com>
2025-04-18 02:34:49 +0900

    .

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,6 @@
+## 0.0.2
+
+- Support platforms that does not have stderr
 ## 0.0.1
 
- - Initial release
+- Initial release
--- lib/global_logger.dart
+++ lib/global_logger.dart
@@ -4,17 +4,21 @@ import 'package:logger/logger.dart';
 // This will show all logs even in release mode.
 class _MyFilter extends LogFilter {
   @override
-  bool shouldLog(LogEvent event) {
+  bool shouldLog(LogEvent $event) {
     return true;
   }
 }
 
-// Output logs to stderr
+// Output logs to stderr (if stderr is not supported just use print()).
 class _MyeOutput extends LogOutput {
   @override
-  void output(OutputEvent event) {
-    for (var line in event.lines) {
-      io.stderr.writeln(line);
+  void output(OutputEvent $event) {
+    for (var $line in $event.lines) {
+      try {
+        io.stderr.writeln($line);
+      } catch(e) {
+        print($line);
+      }
     }
   }
 }
